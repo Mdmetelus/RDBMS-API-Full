14c67345bb0e669f00daece444732d30bc313930
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-16 17:26:20 -0500

    "update"

--- seeds/001-4:57-1:16:19-cohort-seeds.js
+++ seeds/001-4:57-1:16:19-cohort-seeds.js
@@ -1,13 +1,22 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cohorts').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cohorts').insert([
+        {name: 'rowValue1'},
+        {name: 'rowValue2'},
+        {name: 'rowValue3'},
+        {name: 'rowValue4'},
+        {name: 'rowValue5'},
+        {name: 'rowValue6'},
+        {name: 'rowValue7'},
+        {name: 'rowValue8'},
+        {name: 'rowValue9'},
+        {name: 'rowValue10'},
+        {name: 'rowValue11'},
+        {name: 'rowValue12'}
       ]);
     });
 };
--- seeds/001-4:59-1:16:19-students-seeds.js
+++ seeds/001-4:59-1:16:19-students-seeds.js
@@ -1,13 +1,29 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('table_name').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {name: '1rowValue1', cohort_id: 1,},
+        {name: '2rowValtyrue2',cohort_id: 1,},
+        {name: '3rowVq34alue3', cohort_id: 1,},
+        {name: '4rowVa5eylue4', cohort_id: 7,},
+        {name: '5rowVaukrlue5', cohort_id: 2,},
+        {name: '6rowVtyalue6', cohort_id: 8,},
+        {name: '7rgghowValue7', cohort_id: 2,},
+        {name: '8rowValugsfghe8', cohort_id: 2,},
+        {name: '9rowggsfValue9', cohort_id: 3,},
+        {name: '10rogswValue10', cohort_id: 9,},
+        {name: '11rowVagsglue11', cohort_id: 3,},
+        {name: '12rowVgsgalue12', cohort_id: 4,},
+        {name: '13rowVhgsgalue6', cohort_id: 5,},
+        {name: '14rowValue7', cohort_id: 10,},
+        {name: '15rowValsghgue8', cohort_id: 4,},
+        {name: '16rowValuhsgdhse9', cohort_id: 11,},
+        {name: '17rowhsh', cohort_id: 4,},
+        {name: '18rowVsgghalue11', cohort_id: 12,},
+        {name: '19roweyValujke12', cohort_id: 5,}
       ]);
     });
 };
