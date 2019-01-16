e0c54d518463db8dc8090827ec61b461bbe8cc77
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-16 16:33:11 -0500

    "cohort migration created"

--- migrations/20190116161326_cohorts.js
+++ migrations/20190116161326_cohorts.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
+    //makes changes to our database
+    return knex.schema.createTable('cohorts', function(tbl){
+        //primary key (will always be id) empty = id; 'items_id' = sspecifically named
+        tbl.increments();
+
+        tbl.string('name', 128);
+
+        //timestamp
+        tbl.timestamp(true,true);
+
+    }
   
 };
 
 exports.down = function(knex, Promise) {
-  
+    // rollback/undo the changes/ undo the created database from above
+
+    knex.schema.dropTableIfExists('cohorts');
 };
