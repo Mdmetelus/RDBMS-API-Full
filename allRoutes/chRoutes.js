557f6f4aa8416c365e06090feebb72b1440689ca
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-16 16:02:39 -0500

    " delete route created"

--- allRoutes/chRoutes.js
+++ allRoutes/chRoutes.js
@@ -5,11 +5,12 @@ const db = knex(knexConfig.development);
 console.log(knexConfig);
 const router = express.Router();
 
-
-
 //++++++++++++++++++++++++++++++++++++++++++
-// get endpoints
+//   -   ALL ROUTES     -
+//++++++++++++++++++++++++++++++++++++++++++
+// - Get Endpoints. 
 //++++++++++++++++++++++++++++++++++++++++++++
+
 router.get('/', (req, res) => {
     console.log('get all request working');
     db('cohorts')
@@ -27,8 +28,9 @@ router.get('/', (req, res) => {
 
 
 //++++++++++++++++++++++++++++++++++++++++
-// - post stuff here
+// - Post Endpoint.     -
 //++++++++++++++++++++++++++++++++++++++++
+
 router.post('/', (req, res) => {
     console.log('post request working');
     const content = req.body;
@@ -42,6 +44,31 @@ router.post('/', (req, res) => {
             res.status(500).json({error: "There was an error while saving your cohort info", err});
         });
 });
+
+
+//++++++++++++++++++++++++++++++++++++++++
+// - Delete Endpoint.   -
+//++++++++++++++++++++++++++++++++++++++++
+// Calling .del() without first filtering the records will result on the removal of all the records in the table, be careful!
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('cohorts')
+        .where({ id })
+        .del()
+        .then(count => {
+            if (count == 0) {
+                res.status(404).json({ error: 'Error, nothing was deleted. Please try again.', err});
+            } else {
+                res.status(200).json(count);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'There was an error deleting the cohort. Please Try again.', err }) 
+        });
+});
+
 
 
 
